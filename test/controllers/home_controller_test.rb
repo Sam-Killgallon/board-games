5bd48b8b3f929b0cb8b433a987e879fb15708312
Sam Killgallon <sam.killgallon@gmail.com>
2019-02-10 19:43:18 +0000

    Add sign up and sign in text to home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -7,4 +7,14 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test 'should contain sign up link' do
+    get root_url
+    assert_match 'Sign up', @response.body
+  end
+
+  test 'should contain sign in link' do
+    get root_url
+    assert_match 'Sign in', @response.body
+  end
 end
