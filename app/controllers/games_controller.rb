f4060237950ba54fef4657038ee4181380c3e1cf
Sam Killgallon <sam.killgallon@gmail.com>
2019-03-16 16:44:24 +0000

    Allow users to select games they own

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class GamesController < ApplicationController
+  before_action :require_user!
+
+  def index
+    @games = current_user.games
+  end
+
+  def new
+    # Only return list games that the user doesn't already have
+    @games = Game.all - current_user.games
+  end
+
+  def create
+    games = Game.find(params[:games])
+    current_user.games << games
+
+    redirect_to games_path
+  end
+
+  def destroy
+    current_user.games.destroy(params[:id])
+    redirect_to games_path, notice: 'Game was successfully destroyed.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
 
+  resources :games, only: %i[index new create destroy]
+
   namespace :admin do
     root 'home#index'
     resources :games
