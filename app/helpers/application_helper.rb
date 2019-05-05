684c9a931bb9ea3c7bc3ea1892c13e38ac664aa0
Sam Killgallon <sam.killgallon@gmail.com>
2019-05-05 17:27:44 +0100

    Add nav_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def nav_link_to(name, path, html_options = {})
+    classes = html_options.fetch(:class, [])
+    classes << 'active' if current_page?(path)
+    html_options[:class] = classes
+
+    link_to(name, path, html_options)
+  end
+
   def scheduled_time_in_words(time)
     return 'Unscheduled' unless time
 
