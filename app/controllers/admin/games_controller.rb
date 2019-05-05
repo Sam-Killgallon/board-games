940867eb6d7ba544a5ed941ce9a7e2e91dfc0c39
Sam Killgallon <sam.killgallon@gmail.com>
2019-05-05 13:32:58 +0100

    Remove route comments

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -3,23 +3,18 @@
 class Admin::GamesController < Admin::ApplicationController
   before_action :set_game, only: %i[show edit update destroy]
 
-  # GET /admin/games
   def index
     @games = Game.all
   end
 
-  # GET /admin/games/1
   def show; end
 
-  # GET /admin/games/new
   def new
     @game = Game.new
   end
 
-  # GET /admin/games/1/edit
   def edit; end
 
-  # POST /admin/games
   def create
     @game = Game.new(game_params)
 
@@ -31,7 +26,6 @@ class Admin::GamesController < Admin::ApplicationController
     end
   end
 
-  # PATCH/PUT /admin/games/1
   def update
     if @game.update(game_params)
       redirect_to [:admin, @game], notice: 'Game was successfully updated.'
@@ -40,7 +34,6 @@ class Admin::GamesController < Admin::ApplicationController
     end
   end
 
-  # DELETE /admin/games/1
   def destroy
     @game.destroy
     redirect_to admin_games_path, notice: 'Game was successfully destroyed.'
@@ -48,12 +41,10 @@ class Admin::GamesController < Admin::ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_game
     @game = Game.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def game_params
     params.require(:game).permit(:title, :min_players, :max_players, :box_image)
   end
