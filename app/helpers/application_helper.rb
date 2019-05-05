15842b198141dd9f0e8c769afac83483f1792891
Sam Killgallon <sam.killgallon@gmail.com>
2019-05-05 12:54:30 +0100

    Refactor views a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def scheduled_time_in_words(time, context: nil)
+  def scheduled_time_in_words(time)
     return 'Unscheduled' unless time
 
-    case context
-    when :future   then "In #{time_ago_in_words(time)}"
-    when :past     then "#{time_ago_in_words(time)} ago"
-    when :flexible then flexible_time(time)
-    else time_ago_in_words(time)
-    end
-  end
-
-  private
-
-  def flexible_time(time)
     if Time.current > time
-      scheduled_time_in_words(time, context: :past)
+      "#{time_ago_in_words(time)} ago"
     else
-      scheduled_time_in_words(time, context: :future)
+      "In #{time_ago_in_words(time)}"
     end
   end
 end
